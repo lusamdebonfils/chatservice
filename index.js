47ac146771d03675839b9b262daade8032067adf
lusamdebonfils <samuelluswata@gmail.com>
2019-11-18 13:26:37 -0600

    Initial commit for dev purposes

---
+++ index.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var socket = require('socket.io')
+
+//App setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log('Listening to requests on port 4000');
+});
+
+//Static files
+app.use(express.static('public'));
+
+
+//socket setup
+var io = socket(server);
+
+
+io.on('connection',(socket) => {
+    console.log('made socket connection', socket.id);
+    socket.on('chat',(data)=>{
+        io.sockets.emit('chat',data);
+    });
+
+    socket.on('typing',(data)=>{
+        socket.broadcast.emit('typing',data);
+    });
+
+});
+/*
+
+
+
+const chatRooms = ['nba','basketball','bedminton'];
+
+io.of('/chat').on('connection',(socket)=>{
+    console.log('New user has been connected');
+    socket.emit('welcome','hello players yall welcome');
+
+    socket.on('joinRoom',(room)=>{
+        if(chatRooms.includes(room)){
+            socket.join(room);
+            //io.of('/nba').emit('newUser', 'New Player has joined the room');
+            io.of('/nba').in(room).emit('newUser', 'New Player has joined the ' + room + ' room');
+            return socket.emit('success','You successful joined the '+ room + ' room')
+        }else{
+            return socket.emit('err','Error, No room named '+ room)
+        }
+        
+    });
+});
+
+
+*/
---
+++ public/chat.js
@@ -0,0 +1,56 @@
+//Make connection
+
+var socket = io.connect('http://localhost:4000');
+
+//Query DOM
+// Query DOM
+var message = document.getElementById('message'),
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output'),
+      feedback = document.getElementById('feedback');
+
+//Emit Events
+
+btn.addEventListener('click',()=>{
+    // alert('Loaded');
+    socket.emit('chat',{
+        message : message.value,
+        handle : handle.value
+    });
+    
+});
+
+message.addEventListener('keypress',()=>{
+    socket.emit('typing', handle.value);
+});
+
+//Listen for events
+socket.on('chat',(data)=>{
+    feedback.innerHTML = '';
+    //message.value = '';
+    output.innerHTML += '<p><strong>' + data.handle + ' : </strong>' + data.message + '</p>';
+});
+
+socket.on('typing',(data)=>{
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
+
+socket.on('welcome',(data)=>{
+    console.log('message received', data);
+});
+//emit to ask join the chatroom
+socket.emit('joinRoom','nba');
+
+socket.on('newUser',(data)=>{
+    console.log(data);
+});
+
+socket.on('err',(err)=>{
+    console.log(err);
+});
+
+socket.on('success',(data)=>{
+    console.log(data);
+});
+
